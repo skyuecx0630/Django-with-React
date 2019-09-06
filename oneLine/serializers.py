031dc96521a3c169acac2451b0071890bfe655f7
hmoon630 <s19017@gsm.hs.kr>
2019-09-06 11:51:56 +0900

    Initial commit

---
+++ oneLine/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import WiseSaying
+
+class WiseSayingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WiseSaying
+        fields = '__all__'
